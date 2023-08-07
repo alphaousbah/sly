d1fdff6f5f4791322026233f8a213d37bd5a017a
alphaousbah <alphaousbah@gmail.com>
2023-08-07 20:54:56 +0200

    Change call to app

--- app/__init__.py
+++ app/__init__.py
@@ -3,23 +3,23 @@ from config import BaseConfig
 
 
 def create_app():
-    app = Flask(__name__)
-    app.config.from_object(BaseConfig)
+    server = Flask(__name__)
+    server.config.from_object(BaseConfig)
 
-    register_extensions(app)
-    register_blueprints(app)
+    register_extensions(server)
+    register_blueprints(server)
 
-    return app
+    return server
 
 
-def register_extensions(app):
+def register_extensions(server):
     from app.extensions import db, migrate
 
-    db.init_app(app)
-    migrate.init_app(app)
+    db.init_app(server)
+    migrate.init_app(server)
 
 
-def register_blueprints(app):
+def register_blueprints(server):
     from app.views.home import home
 
-    app.register_blueprint(home)
+    server.register_blueprint(home)
