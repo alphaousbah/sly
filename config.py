f9e29151088f30d7cbf8fa2358abec035755cac9
alphaousbah <alphaousbah@gmail.com>
2023-08-08 23:58:14 +0200

    Fix

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ class AzureConfig:
     ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
     CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
-    # Configure Postgres database based on connection string of the libpq Keyword/Value form
+    # Configure Postgres database based on connection string
     # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
     conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
     conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
