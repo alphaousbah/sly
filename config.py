7c71f2ea6a2643a9d0eb74cf1ca76447c7653978
alphaousbah <alphaousbah@gmail.com>
2023-08-11 13:21:39 +0200

    Fix

--- config.py
+++ config.py
@@ -6,29 +6,27 @@ class Config:
     SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # WEBSITE_HOSTNAME exists only in production environment
-    if 'WEBSITE_HOSTNAME' not in os.environ:
-        # Local deployment : we'll use environment variables
-        SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-            dbuser=os.environ['DBUSER'],
-            dbpass=os.environ['DBPASS'],
-            dbhost=os.environ['DBHOST'],
-            dbname=os.environ['DBNAME']
-        )
+    # # WEBSITE_HOSTNAME exists only in production environment
+    # if 'WEBSITE_HOSTNAME' not in os.environ:
+    #     # Local deployment : we'll use environment variables
+    #     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+    #         dbuser=os.environ['DBUSER'],
+    #         dbpass=os.environ['DBPASS'],
+    #         dbhost=os.environ['DBHOST'],
+    #         dbname=os.environ['DBNAME']
+    #     )
+    #
+    # else:
+    # Cloud deployment
+    conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+    conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 
-    else:
-        # Production
-        # Configure Postgres database based on connection string of the libpq Keyword/Value form
-        # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
-        conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-        conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
-
-        SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-            dbuser=conn_str_params['user'],
-            dbpass=conn_str_params['password'],
-            dbhost=conn_str_params['host'],
-            dbname=conn_str_params['dbname']
-        )
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+        dbuser=conn_str_params['user'],
+        dbpass=conn_str_params['password'],
+        dbhost=conn_str_params['host'],
+        dbname=conn_str_params['dbname']
+    )
 
 
 class SQLiteConfig:
