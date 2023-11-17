f00dab2ab7ef0b0a3e6eceb3d0dab3f44098aa5d
alphaousbah <alphaousbah@gmail.com>
2023-11-17 21:23:26 +0100

    view result

--- flaskapp/dashapp/pages/results/view.py
+++ flaskapp/dashapp/pages/results/view.py
@@ -1,7 +1,8 @@
-import dash
+# https://stackoverflow.com/questions/9916878/importing-modules-in-python-best-practice/29193752#29193752import dash
 from dash import html, dcc, dash_table, callback, Output, Input, State, ALL
 from dash.exceptions import PreventUpdate
 import dash_bootstrap_components as dbc
+import dash_ag_grid as dag
 import dash_mantine_components as dmc
 from flaskapp.dashapp.pages.utils import *
 from flaskapp.extensions import db
@@ -17,38 +18,65 @@ page_id = get_page_id(__name__)
 def layout(analysis_id, resultfile_id=None):
     analysis = db.session.get(Analysis, analysis_id)
 
-    if resultfile_id is None:
-        resultfile_id = db.session.query(ResultFile).order_by(ResultFile.id.desc())
+    # Initialize the OEP and summary tables
+    QUANTILES = [.999, .998, .996, .995, .99, .98, .9667, .96, .95, .9, .8, .5]
+    df_oep = pd.DataFrame({
+        'quantile': [f'{quantile:.2%}' for quantile in QUANTILES],
+        'return period': [f'{1 / (1 - quantile):,.0f}' for quantile in QUANTILES],
+        'proba': QUANTILES,
+    })
+    df_summary = pd.DataFrame({
+        'quantile': ['Pure premium', 'Standard deviation'],
+        'return period': ['', ''],
+        'proba': ['', ''],
+    })
 
-    resultfile = db.session.get(ResultFile, resultfile_id)
+    if resultfile_id:
+        resultfile = db.session.get(ResultFile, resultfile_id)
+    else:
+        resultfile = db.session.query(ResultFile).order_by(ResultFile.id.desc()).first()
 
-    # Get the layers of the result pricing relationship
-    layertomodelfiles = resultfile.pricingrelationship.layertomodelfiles
-    # Use set to get the layers without repetition
-    # https://www.learnpython.org/en/Sets
-    layers = set([layertomodelfile.layer for layertomodelfile in layertomodelfiles])
-    resultyearlosses = resultfile.resultyearlosses
+    if resultfile:
+        # Set the title of the page
+        title = resultfile.name.capitalize()
 
-    # Initialize the OEP dataframe
-    QUANTILES = [.999, .998, .996, .995, .99, .98, .966667, .96, .95, .9, .8, .5, .1]
-    df_oep = pd.DataFrame({'quantile': QUANTILES})
+        # Get the layers of the result pricing relationship
+        layertomodelfiles = resultfile.pricingrelationship.layertomodelfiles
+        # Use set to get the layers without repetition
+        # https://www.learnpython.org/en/Sets
+        layers = [layertomodelfile.layer for layertomodelfile in layertomodelfiles]
 
-    for layer in layers:
-        # Get the year losses for the current layer
-        layer_resultyearlosses = [
-            resultyearloss for resultyearloss in resultyearlosses
-            if resultyearloss.layertomodelfile.layer == layer
-        ]
-        recoveries = df_from_query(layer_resultyearlosses)[['year', 'recovery']]
-        recoveries['recovery'] = recoveries['recovery'].astype(int)
+        # Sort the layers by name
+        layers = sorted(layers, key=lambda l: l.name)
 
-        recoveries_by_year = recoveries.groupby('year')['recovery'].sum()
-        df_oep[layer.name] = df_oep['quantile'].map(lambda proba: round(recoveries_by_year.quantile(proba)))
+        # Get the result year losses
+        resultyearlosses = resultfile.resultyearlosses
 
-        print(df_oep)
+        for layer in layers:
+            # Get the year losses for the current layer
+            layer_resultyearlosses = [
+                resultyearloss for resultyearloss in resultyearlosses
+                if resultyearloss.layertomodelfile.layer == layer
+            ]
+            recoveries = df_from_query(layer_resultyearlosses)[['year', 'recovery']]
+            recoveries['recovery'] = recoveries['recovery'].astype(int)
+
+            recoveries_by_year = recoveries.groupby('year')['recovery'].sum()
+            df_oep[layer.name] = df_oep['proba'].map(lambda proba: f'{recoveries_by_year.quantile(proba):,.0f}')
+
+            df_summary[layer.name] = [
+                # Pure premium
+                f'{recoveries_by_year.mean():,.0f}',
+                # Standard deviation
+                f'{recoveries_by_year.std():,.0f}',
+            ]
+
+    else:
+        # Set the title of the page
+        title = 'Process a pricing model to get the results'
 
     return html.Div([
-        dcc.Store(id=page_id + 'store', data={'analysis_id': analysis_id, 'result_id': resultfile_id}),
+        dcc.Store(id=page_id + 'store', data={'analysis_id': analysis_id, 'resultfile_id': resultfile_id}),
         get_title(__name__, analysis.name),
         get_nav_middle(__name__, analysis.id),
         get_nav_bottom(__name__, analysis.id),
@@ -56,42 +84,23 @@ def layout(analysis_id, resultfile_id=None):
         html.Div([
             dbc.Row([
                 dbc.Col([
-                    dag.AgGrid(
-                        id=page_id + 'grid-oep',
-                        rowData=df_oep.to_dict('records'),
-                        columnDefs=[
-                            {'field': 'id', 'hide': True},
-                            {
-                                'field': 'name',
-                                'checkboxSelection': True, 'headerCheckboxSelection': True,
-                                'rowDrag': True,
-                            },
-                            {'field': 'premium', 'valueFormatter': {'function': 'd3.format(",d")(params.value)'}, },
-                            {'field': 'deductible', 'valueFormatter': {'function': '(params.value) + "%"'}, },
-                            {'field': 'limit', 'valueFormatter': {'function': '(params.value) + "%"'}, },
-                            {'field': 'analysis_id'},
-                            {'field': 'display_order'},
-                            # {'field': 'display_order', 'hide': True},
-                        ]
-                        ,
-                        getRowId='params.data.id',
-                        defaultColDef={
-                            'flex': True,
-                            'editable': True,
-                        },
-                        dashGridOptions={'rowSelection': 'multiple', 'rowDragManaged': True, 'rowDragMultiRow': True,
-                                         'animateRows': True},
-                        persistence=True,
-                        className='ag-theme-alpine custom mb-2',
-                    ),
+                    html.Div(title, className='h6 mb-3'),
                 ]),
             ]),
             dbc.Row([
                 dbc.Col([
-
-                ]),
-                dbc.Col([
-
+                    # https://dash.plotly.com/dash-ag-grid/row-pinning
+                    dag.AgGrid(
+                        id=page_id + 'grid-oep',
+                        rowData=df_oep.to_dict('records'),
+                        columnDefs=[{'field': col} for col in df_oep.columns if col != 'proba'],
+                        dashGridOptions={
+                            'rowHeight': 40,
+                            'pinnedBottomRowData': df_summary.to_dict('records'),
+                        },
+                        style={'height': 500},
+                        className='ag-theme-alpine custom mb-2',
+                    ),
                 ]),
             ]),
             dbc.Row([
